548b459b28c1efb20458ee5be1d9e52c227a6130
fazalfarhan01 <fazal.farhan@gmail.com>
2021-05-28 21:33:07 +0530

    pretty printed app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ import imutils
 import time
 
 # Setting custom Page Title and Icon with changed layout and sidebar state
-st.set_page_config(page_title='Face Mask Detector', page_icon='😷', layout='centered', initial_sidebar_state='expanded')
+st.set_page_config(page_title='Face Mask Detector', page_icon='😷',
+                   layout='centered', initial_sidebar_state='expanded')
 
 
 def local_css(file_name):
@@ -99,73 +100,67 @@ def mask_image():
             RGB_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
-
-
 def detect_and_predict_mask(frame, faceNet, maskNet):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
 
-	# pass the blob through the network and obtain the face detections
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
 
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
-	faces = []
-	locs = []
-	preds = []
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > args["confidence"]:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
 
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
-	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
 
-
-faceNet = 0
-maskNet = 0
-vs = 0
 
 def mask_video(args):
     global faceNet, maskNet, vs
@@ -173,7 +168,7 @@ def mask_video(args):
     print("[INFO] loading face detector model...")
     prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
     weightsPath = os.path.sep.join([args["face"],
-        "res10_300x300_ssd_iter_140000.caffemodel"])
+                                    "res10_300x300_ssd_iter_140000.caffemodel"])
     faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
     # load the face mask detector model from disk
@@ -188,38 +183,45 @@ def mask_video(args):
 
     time.sleep(2.0)
 
+
 def mask_detection(args):
     local_css("css/styles.css")
-    st.markdown('<h1 align="center">😷 Face Mask Detection</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 align="center">😷 Face Mask Detection</h1>',
+                unsafe_allow_html=True)
     activities = ["Image", "Webcam"]
     # activities = ["Image"]
     st.set_option('deprecation.showfileUploaderEncoding', False)
     st.sidebar.markdown("# Mask Detection on?")
-    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    choice = st.sidebar.selectbox(
+        "Choose among the given options:", activities)
 
     if choice == 'Image':
-        st.markdown('<h2 align="center">Detection on Image</h2>', unsafe_allow_html=True)
+        st.markdown('<h2 align="center">Detection on Image</h2>',
+                    unsafe_allow_html=True)
         st.markdown("### Upload your image here ⬇")
-        
+
         image_file = st.file_uploader("", type=["jpg"])  # upload image
         if image_file is not None:
             our_image = Image.open(image_file)  # making compatible to PIL
             im = our_image.save('./images/out.jpg')
-            saved_image = st.image(image_file, caption='', use_column_width=True)
-            st.markdown('<h3 align="center">Image uploaded successfully!</h3>', unsafe_allow_html=True)
+            saved_image = st.image(
+                image_file, caption='', use_column_width=True)
+            st.markdown(
+                '<h3 align="center">Image uploaded successfully!</h3>', unsafe_allow_html=True)
             if st.button('Process'):
                 mask_image()
                 st.image(RGB_img, use_column_width=True)
 
     if choice == 'Webcam':
         mask_video(args)
-        st.markdown('<h2 align="center">Detection on Webcam</h2>', unsafe_allow_html=True)
+        st.markdown('<h2 align="center">Detection on Webcam</h2>',
+                    unsafe_allow_html=True)
         # st.markdown('<h3 align="center">This feature will be available soon!</h3>', unsafe_allow_html=True)
         image_placeholder = st.empty()
         if st.button('Start'):
             while True:
-            # grab the frame from the threaded video stream and resize it
-            # to have a maximum width of 400 pixels
+                # grab the frame from the threaded video stream and resize it
+                # to have a maximum width of 400 pixels
                 frame = vs.read()
                 frame = imutils.resize(frame, width=480, height=360)
 
@@ -238,28 +240,31 @@ def mask_detection(args):
                     # the bounding box and text
                     label = "Mask" if mask > withoutMask else "No Mask"
                     color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-                        
+
                     # include the probability in the label
-                    label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+                    label = "{}: {:.2f}%".format(
+                        label, max(mask, withoutMask) * 100)
 
                     # display the label and bounding box rectangle on the output
                     # frame
                     cv2.putText(frame, label, (startX, startY - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                    cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                    cv2.rectangle(frame, (startX, startY),
+                                  (endX, endY), color, 2)
 
                 image_placeholder.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     st.text("By:\n- Mohamed Farhan Fazal\n- Madhusudhan R\n- Kiran CR")
 
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--face", type=str,
-    default="face_detector",
-    help="path to face detector model directory")
+                default="face_detector",
+                help="path to face detector model directory")
 ap.add_argument("-m", "--model", type=str,
-    default="mask_detector.model",
-    help="path to trained face mask detector model")
+                default="mask_detector.model",
+                help="path to trained face mask detector model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
-    help="minimum probability to filter weak detections")
+                help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 mask_detection(args)
